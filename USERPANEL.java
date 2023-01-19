80e613cc1b38de75d181a30809730dab5288a50b
Willis Razak <willis.razak@mrjaffesclass.com>
2023-01-19 10:33:48 -0800

    finally. cheese.

--- USERPANEL.java
+++ USERPANEL.java
@@ -1,33 +1,59 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
+import java.util.concurrent.ThreadLocalRandom;
 
 // it's what the user uses.
 public class USERPANEL extends gameboard
 {
     int[] guess = new int[4];
-    String[] pegs = new String[4];
+    
     private int[] solution = new int[4];
     Scanner scanner = new Scanner(System.in);
+    boolean CONTINUE = true;
+    int DIDIT = 0;
+    int TRIES = 0;
     public void EVENTLOOP() {
         codegen(solution);
         explaining();
         scanner.useDelimiter(",|\n");
-        while (true){
-            System.out.println("your guess? no spaces, just 4 numbers seperated by commas.");
+        while (CONTINUE==true){
+            System.out.println("your guess? no spaces, just 4 numbers seperated by commas. numbers can repeat.");
+            DIDIT = 0;
+            String[] pegs = new String[4];
             try{
-                for(int scan = 0; 0<=4;scan++){
-                  guess[scan] = scanner.nextInt();  
+                for(int scan = 0; scan<=3;scan++){
+                    guess[scan] = scanner.nextInt();
+                        if(((String.valueOf(guess[scan]).length())>1)||(guess[scan]>3)){
+                        System.out.println("nope. try again.");
+                        System.out.println("remember: commas. single digits, from 0-3.");
+                        scanner.next();
+                    }
+                }
+                
+                System.out.println("let's see the accuracy:");
+                accuracy(guess,solution,pegs);
+                System.out.println(pegs[0]+pegs[1]+pegs[2]+pegs[3]);
+                TRIES++;
+                for (int g = 0; g<=3; g++){
+                    if (pegs[g].equals("yep, ")){
+                        DIDIT++;
+                    }
+                }
+                if(DIDIT==4){
+                    System.out.println("You did it! all it took was "+TRIES+" attempts.");
+                    System.out.println("goodbye.");
+                    CONTINUE= false;
                 }
             } catch (InputMismatchException error){
                 System.out.println("enter correctly.");
                 System.out.println("example: 4,3,2,5");
                 scanner.next();
             }
-            System.out.println("let's see the accuracy:");
             
         }
-        
+
     }
+
     public static void main (String[] args) {
         USERPANEL userpanel = new USERPANEL();
         userpanel.EVENTLOOP();
--- gameboard.java
+++ gameboard.java
@@ -1,37 +1,46 @@
 //computer who does logic
+import java.util.concurrent.ThreadLocalRandom;
 public class gameboard
 {
     public int[] codegen(int[] code){
         for(int i=0;i<=3; i++){
-            code[i]= (int)(Math.random()*6);
+            code[i]= ThreadLocalRandom.current().nextInt(0,4);
         }
         return code;
     }
+
     public void explaining() {
         System.out.println("welcome to this janky mess of a project.");
         System.out.println("an attempt to explain the rules of mastermind:");
-        System.out.println("you want to guess my code of 4 numbers, from 1-6.");
-        System.out.println("you have a limited amount of guesses, and a limited amount of info from guesses.");
+        System.out.println("you want to guess my code of 4 numbers, each number being from  0-3.");
+        System.out.println("like a padlock. each space can be from 0-3, and numbers can repeat.");
+        System.out.println("you have an unlimited amount of guesses,but a limited amount of info from guesses.");
         System.out.println("i'll tell you if a correct number is in the right place, or if it's a correct number in the wrong place.");
         System.out.println("good luck.");
         System.out.println("");
     }
-    public void accuracy(int[] guess, int[] real ){
-        System.out.println("");
-        int f=0;
+
+    public void accuracy(int[] guess, int[] real, String[] pegs){
         boolean[] taken = new boolean[4];
+        for(int g = 0; g<=3; g++){
+            taken[g] = false;
+        }
         for(int step=0; step<=3; step++){
-            if (guess[step] == real[step]){
-                taken[step]=true;
-                System.out.print("yep, ");
-            } else {
-                for (int i=0; i<guess.length; i++){
-                    if (guess[i] ==real[f]&&taken[i]==false){
-                      System.out.print("close, ");  
-                      f++;
+            if ((guess[step])==(real[step])&&(taken[step]==false)){
+                taken[step]=true;   
+                pegs[step]=("yep, ");
+            } else{
+                for (int i=0; i<=3; i++){
+                    if ((guess[step]==real[i])&&(taken[i]==false)&&(pegs[i]!=("yep, "))){
+                        pegs[step]=("close, ");
                     }
                 }
             }
+        }
+        for(int g=0; g<=3; g++){
+                if(((taken[g]==false)||(pegs[g]==null))&&(pegs[g]!=("close, "))){
+                    pegs[g]=("no, ");
+                }
         }
     }
 }
